0eec9f2bf95236c091da45b2d68114231e2adcf9
Obelem <ibbraidejesse@gmail.com>
2023-01-29 19:05:37 +0100

    made pycodestyle compliant

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,35 +3,36 @@
 
 
 def pascal_triangle(n):
-  """returns a list of lists of integers representing the Pascal’s triangle of n
+    """returns a list of lists of integers representing the Pascal’s triangle
+    of n
 
-  Args:
-      n (n): size of triangle
+    Args:
+        n (n): size of triangle
 
-  Returns:
-      list: returns empty list if n <= 0 or list of lists of integers
-      representing Pascal's triangle of n otherwise
-  """
-  if n <= 0:
-    return []
-  
-  triangle, buffer_list = [], []
-  for x in range(n):
-    if x == 0:
-      triangle.append([1])
-      continue
-    if x == 1:
-      triangle.append([1, 1])
-      continue
-    
-    sum_list = triangle[-1]
-    for i in range(len(sum_list) + 1):
-      if i in [0, len(sum_list)]:
-        buffer_list.append(1)
-        continue
-      buffer_list.append(sum_list[i] + sum_list[i - 1])
-    
-    triangle.append(buffer_list)
-    buffer_list = []
-    
-  return triangle
+    Returns:
+        list: returns empty list if n <= 0 or list of lists of integers
+        representing Pascal's triangle of n otherwise
+    """
+    if n <= 0:
+        return []
+
+    triangle, buffer_list = [], []
+    for x in range(n):
+        if x == 0:
+            triangle.append([1])
+            continue
+        if x == 1:
+            triangle.append([1, 1])
+            continue
+
+        sum_list = triangle[-1]
+        for i in range(len(sum_list) + 1):
+            if i in [0, len(sum_list)]:
+                buffer_list.append(1)
+                continue
+            buffer_list.append(sum_list[i] + sum_list[i - 1])
+
+        triangle.append(buffer_list)
+        buffer_list = []
+
+    return triangle
